4dce126bcee7633b4839aaa16b4d2e4802e05a6b
Anthony Behery <anthonybehery@Davids-MacBook-Air.local>
2021-09-17 17:27:10 -0500

    Print stock name

--- printcurrentprice.py
+++ printcurrentprice.py
@@ -6,5 +6,11 @@ from yahoo_fin import stock_info as si
 stock = input(
     "Which stock would you want me to retrieve the live price for?: ")
 
+# Print the stock name from the abbreviation
+stock_abrev = yf.Ticker(stock)
+
+company_name = stock_abrev.info['longName']
+
 # Print out the current market price of the stock, rounds to the hundredth's place
+print(company_name)
 print(round(si.get_live_price(stock), 2))

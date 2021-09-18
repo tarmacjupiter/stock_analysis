b05972bee2d033021a08a93f0077eac62879f714
Anthony Behery <anthonybehery@Davids-MacBook-Air.local>
2021-09-18 01:13:39 -0500

    Read string instead of index

--- printcurrentprice.py
+++ printcurrentprice.py
@@ -1,30 +1,26 @@
 import yfinance as yf
 from yahoo_fin import stock_info as si
-from csv import reader
+from csv import DictReader, reader
 
 # skip over the header of the cvs file
-with open('nyse-listed_csv.csv', 'r') as read_obj:
-    csv_reader = reader(read_obj)
-    header = next(csv_reader)
+with open('modelworksheet.csv', 'r') as read_obj:
+    csv_dict_reader = DictReader(read_obj)
     # Check if the file is empty
-    if header != None:
-        # Iterate over each row after the header in the cvs
-        for row in csv_reader:
-            # row variable is a list that represents a row in cvs
-            print(row[0])
+    # Iterate over each row after the header in the cvs
+    for row in csv_dict_reader:
+        # row variable is a list that represents a row in cvs
+
+        stock_abrev = yf.Ticker(row["Ticker"])
+
+        # Logic for getting extended name of ACT symbol, not fully functional
+        company_name = stock_abrev.info['longName']
+
+        print(company_name + ":")
+
+        # # Print out the current market price of the stock, rounds to the hundredth's place
+        # # (yahoo_fin)
+        print(round(si.get_live_price(row["Ticker"]), 2))
+        print("")
 
 
-# # Ask for which stock the user would like to see the current price for
-# stock = input(
-#     "Which stock would you want me to retrieve the live price for?: ")
-
-# Print the stock name from the abbreviation
-# (yfinance)
-# stock_abrev = yf.Ticker(stock)
-
-# company_name = stock_abrev.info['longName']
-
-# # Print out the current market price of the stock, rounds to the hundredth's place
-# # (yahoo_fin)
-# print(company_name + ":")
-# print(round(si.get_live_price(stock), 2))
+print("All done!")
